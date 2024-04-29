5f2eac8e6ef326e3b8b40bb5c11d35fc1c5908b6
reality_user <jc3124567@gmail.com>
2024-04-29 13:20:47 -0500

    added blocks

--- Let's Build GPT From Scratch/pass1/v2.py
+++ Let's Build GPT From Scratch/pass1/v2.py
@@ -102,6 +102,21 @@ class FeedForward(nn.Module):
     def forward(self, x):
         return self.net(x)
 
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: number of heads
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+
+    def forward(self, x):
+        x = self.sa(x)
+        x = self.ffwd(x)
+        return x
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
     def __init__(self):
@@ -109,8 +124,11 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_heads = MultiHeadAttention(4, n_embd//4) # i.e. 4 heads of 8-dimensional self-attention
-        self.ffwd = FeedForward(n_embd)
+        self.blocks = nn.Sequential(
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+        )
         self.lm_head = nn.Linear(n_embd, vocab_size) # linear layer from embeddings to logits
         
     def forward(self, idx, targets=None):
@@ -120,8 +138,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C) "batch, time, channel" -- really "time" is position in the input block.
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C) -- arange(T) is just tensor([0, 1, 2, ..., T-1])
         x = tok_emb + pos_emb # (B,T,C) -- pos_emb is unsqueezed to (1,T,C) and broadcasted to (B,T,C)  
-        x = self.sa_heads(x) # apply one head of self-attention. (B,T,C)
-        x = self.ffwd(x) # (B,T,C)
+        x = self.blocks(x) # (B,T,C)
         logits = self.lm_head(x) # (B,T,vocab_size)
         
         if targets is None:
